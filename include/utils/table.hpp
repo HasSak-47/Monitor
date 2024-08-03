acb9b1875dcc53d19d5e0c7a23b2ede045bf8cf2
arch machine <lilith.alanis.255@gmail.com>
2024-08-02 18:52:58 -0600

    table?

--- include/utils/table.hpp
+++ include/utils/table.hpp
@@ -1,11 +1,28 @@
+#include <vector>
 #include "render/render.hpp"
 #include "text_area.hpp"
 
 namespace Utility{
 
-class Table : virtual TextArea, public virtual Render::Widget{
+class Table : public virtual Render::Widget{
+private:
+	struct Row : public virtual Utility::TextArea{
+		Row(Table& parent) : _parent(parent) {}
+		std::vector<std::string> _data;
+		Table& _parent;
+
+		void render(Render::Buffer& buf) override;
+	};
+	std::vector<std::string> _names;
+	std::vector<size_t> _expected_width;
+	std::vector<Row> _name_row;
+	std::vector<Row> _rows;
 public:
 	Table(){}
+	void add_col(std::string name);
+	std::vector<std::string>& get_row(size_t id);
+
+	void render(Render::Buffer& buf) override;
 };
 
 }
